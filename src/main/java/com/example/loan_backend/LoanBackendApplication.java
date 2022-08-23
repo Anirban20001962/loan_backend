a28beaefa72cfc62b0e127425c09a25e6aaffdb1
Anirban20001962 <anirbandhakuria@gmail.com>
2022-08-23 08:03:06 +0530

    first commit

---
+++ src/main/java/com/example/loan_backend/LoanBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.loan_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoanBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/loan_backend/LoanBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.loan_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
