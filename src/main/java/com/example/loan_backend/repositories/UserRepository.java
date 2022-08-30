5f67ab758c79532b0a12fb38448f23bc9a62cc42
pavan kothapally <85874797+kothapallypavan@users.noreply.github.com>
2022-08-30 20:34:00 +0530

    queries
    few custom queries

--- src/main/java/com/example/loan_backend/repositories/UserRepository.java
+++ src/main/java/com/example/loan_backend/repositories/UserRepository.java
@@ -1,12 +1,23 @@
 package com.example.loan_backend.repositories;
 
 import com.example.loan_backend.models.User;
+
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends CrudRepository<User, UUID> {
   public Optional<User> findByEmail(String email);
 
   public boolean existsByEmail(String email);
+
+  @Query("select u from User u where u.firstname = ?1")
+  List<User> findByFirstname(String firstname);
+
+  @Query("select u from User u where u.lastname = ?1")
+  List<User> findByLastnamme(String lastname);
+  
 }
