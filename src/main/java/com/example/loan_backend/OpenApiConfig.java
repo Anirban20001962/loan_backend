16760e154547f8fc9879b07f135dc6442468cec5
Ranjankumar666 <typhonazazel@gmail.com>
2022-09-06 09:28:33 +0530

    minor fixes

--- src/main/java/com/example/loan_backend/OpenApiConfig.java
+++ src/main/java/com/example/loan_backend/OpenApiConfig.java
@@ -1,7 +1,9 @@
+package com.example.loan_backend;
+
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 
 @OpenAPIDefinition(info = @Info(title = "Loan", description = "" +
-        "Here is the documentation loan backend, you can use to check the response types and request types."))
+                "Here is the documentation for loan backend, you can use to check the response types and request types."))
 public class OpenApiConfig {
 }
